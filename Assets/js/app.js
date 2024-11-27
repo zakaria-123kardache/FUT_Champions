96319e22c8badc84943bbe6021dd691d666e557d
unknown <zz240614@gmail.com>
2024-11-27 12:03:17 +0100

    make fitscher data on ficher json to Semi page fitscher players whit card styles

--- Assets/js/app.js
+++ Assets/js/app.js
@@ -1,22 +1,50 @@
 
-const clickItem = document.getElementById('clickItem');
-const modal = document.getElementById('myModal');
 
 
-clickItem.addEventListener('click', () => {
-  modal.style.display = 'block';
-  document.body.classList.add('modal-open');
+function f() {
+  fetch('player.json') 
+    .then(res => res.json())
+    .then(data => {
+    
+      const playercard = document.getElementById('playercard-fitsch');
+      playercard.innerHTML = '';
+
+      
+      data.players.forEach(player => {
+        const playerCard = document.createElement('div');
+        playerCard.classList.add('d-flex', 'item2');
+
+        playerCard.innerHTML = `
+          <div class="card-info2">
+            <p class="position">${player.position}</p>
+            <p class="rating">${player.rating}</p>
+            <img src="${player.flag}" alt="flag" class="flag" />
+          </div>
+          <div class="card-image2">
+            <img src="${player.photo}" alt="${player.name}" />
+          </div>
+        `;
+
+        playercard.appendChild(playerCard);
+      });
+    })
+    .catch(error => console.error('Error fetching data:', error));
+}
+
+
+const openModalButton = document.getElementById('openModal');
+const semiPage = document.getElementById('semiPage');
+const closebtn = document.querySelector('.close');
+
+openModalButton.addEventListener('click', () => {
+  semiPage.classList.add('show');
+  f(); 
+});
+
+closebtn.addEventListener('click', () => {
+  semiPage.classList.remove('show');
 });
 
 
-function closeModal() {
-  modal.style.display = 'none';
-  document.body.classList.remove('modal-open');
-}
 
 
-window.onclick = function(event) {
-  if (event.target === modal) {
-    closeModal();
-  }
-}
