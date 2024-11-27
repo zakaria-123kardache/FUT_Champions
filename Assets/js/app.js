9ed81b9e14c8a684297cd7166410f19e22b53b6c
unknown <zz240614@gmail.com>
2024-11-27 21:09:30 +0100

    Creation un bouton pour Add un Player et crea modal pour saisir les information du joueur avec affichage dynamique des player cree dans la section player-fichtcher

--- Assets/js/app.js
+++ Assets/js/app.js
@@ -48,3 +48,88 @@ closebtn.addEventListener('click', () => {
 
 
 
+
+
+function openModal() {
+  var modalElement = document.getElementById('addPlayerModal');
+  var modal = new bootstrap.Modal(modalElement); 
+  modal.show(); 
+}
+
+
+function addPlayer() {
+  const position = document.getElementById('position').value;
+  const Name = document.getElementById('Name').value;
+  const nationality = document.getElementById('nationality').value;
+  const photo = document.getElementById('photo').files[0];
+  const flag = document.getElementById('flag').value;
+  const club = document.getElementById('club').value;
+  const logo = document.getElementById('logo').value;
+  const rating = document.getElementById('rating').value;
+  const pace = document.getElementById('pace').value;
+  const shooting = document.getElementById('shooting').value;
+  const passing = document.getElementById('passing').value;
+  const dribbling = document.getElementById('dribbling').value;
+  const defending = document.getElementById('defending').value;
+  const physical = document.getElementById('physical').value;
+
+  
+  if (!position || !Name || !nationality || !photo || !flag || !rating || !logo || !club || !pace || !shooting || !passing || !dribbling || !defending || !physical) {
+    alert("Please fill in all fields.");
+    return;
+  }
+
+  let imageSrc = "";
+  if (photo) {
+    const reader = new FileReader();
+    reader.onload = function (e) {
+      imageSrc = e.target.result;
+      addCard(position, physical, defending, dribbling, passing, shooting, club, pace, logo, flag, nationality, Name, imageSrc);
+      closeModal();  
+    };
+    reader.readAsDataURL(photo);
+  } else {
+    addCard(position, physical, defending, dribbling, passing, shooting, club, pace, logo, flag, nationality, Name, imageSrc);
+    closeModal();  
+  }
+}
+
+
+function closeModal() {
+  var modalElement = document.getElementById('addPlayerModal');
+  var modal = bootstrap.Modal.getInstance(modalElement); 
+  modal.hide(); 
+}
+
+
+function addCard(position, physical, defending, dribbling, passing, shooting, club, pace, logo, flag, nationality, Name, imageSrc) {
+  const container = document.getElementById('playercard-fitsch');
+  const card =  `
+  
+  <div class="d-flex item2">
+      <div class="card-info2">
+        <p class="position">${position}</p>
+        <p class="rating">${shooting}</p>
+        <img src="${flag}" alt="" class="flag" />
+      </div>
+      <div class="card-image2">
+        <img src="${imageSrc}" alt="" />
+      </div>
+    </div>
+  
+`;
+
+
+    
+  container.innerHTML += card;
+}
+
+
+// function showmodal(){
+//   console.log("jajfkajf")
+//   document.getElementById("modalsecond").style.display = "block"
+// }
+
+
+
+
